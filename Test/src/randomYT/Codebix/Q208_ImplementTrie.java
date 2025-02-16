e8b6c6b2d6723ab5fc79fadb9a8da6e3488a9345
Sbhoi21 <shmbhoi@gmail.com>
2025-02-16 23:45:41 +0530

    implement Trie

---
+++ Test/src/randomYT/Codebix/Q208_ImplementTrie.java
@@ -0,0 +1,28 @@
+package randomYT.Codebix;
+
+public class Q208_ImplementTrie {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public Q208_ImplementTrie() {
+
+	}
+
+	public void insert(String word) {
+
+	}
+
+	public boolean search(String word) {
+		return false;
+	}
+
+	public boolean startsWith(String prefix) {
+
+		return false;
+
+	}
+
+}
